21301cc8fe67c18443de0b581779dfff2db3ce07
DerEros <DerEros@users.noreply.github.com>
2020-01-22 21:34:52 +0100

    Add dummy cucumber test
    Just run with 'dotnet test'

---
+++ Cucumber.Specs/DummySteps.cs
@@ -0,0 +1,28 @@
+using System;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace Cucumber.Specs {
+    [Binding]
+    public class DummySteps {
+        int firstValue = 0;
+        int secondValue = 0;
+
+        [Given(@"a value of (.*)")]
+        public void GivenAValue(int firstValue)
+        {
+            this.firstValue = firstValue;
+        }
+
+        [When(@"comparing it to (.*)")]
+        public void WhenComparingTo(int secondValue)
+        {
+            this.secondValue = secondValue;
+        }
+
+        [Then(@"the values should be equal")]
+        public void ValuesShouldBeEqual() {
+            Assert.True(this.firstValue == this.secondValue);
+        }
+    }
+}
---
+++ Cucumber.Specs/Features/Dummy.feature
@@ -0,0 +1,7 @@
+Feature: Dummy
+    A dummy feature file to test Cucumber with dotnet
+
+Scenario: Perform a successful test
+        Given a value of 50
+        When comparing it to 50
+        Then the values should be equal
